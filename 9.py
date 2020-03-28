c8437a90dff250a42bce978b2357489e92ab7a23
haydhook <53049056+haydhook@users.noreply.github.com>
2020-03-28 20:15:36 +0000

    Create 9.py

---
+++ 9.py
@@ -0,0 +1,13 @@
+target = int ( input ("Triplet Test: "))
+
+for a in range(1, round(target / 3)):
+    #Taking advantage of a < b < c instead of 1,1000
+    for b in range(a, round(target / 2)):
+        #Taking advantage of a < b < c instead of 1,1000
+        c = 1000 - a - b
+        triplet = a * a + b * b == c * c
+        
+        if triplet:
+            print(f'Triplet: {a} {b} {c}\n' +
+                  f'Sum: {a} + {b} + {c} = {a+b+c}\n' +
+                  f'Product: {a} × {b} × {c} = {a*b*c}')
