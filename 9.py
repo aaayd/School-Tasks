8473df755c99699c767844e4e2df5ede288a884b
haydhook <53049056+haydhook@users.noreply.github.com>
2020-03-28 20:16:07 +0000

    Update 9.py

--- 9.py
+++ 9.py
@@ -4,7 +4,7 @@ for a in range(1, round(target / 3)):
     #Taking advantage of a < b < c instead of 1,1000
     for b in range(a, round(target / 2)):
         #Taking advantage of a < b < c instead of 1,1000
-        c = 1000 - a - b
+        c = target - a - b
         triplet = a * a + b * b == c * c
         
         if triplet:
